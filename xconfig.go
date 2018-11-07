b502265a75dad268c73a0db7d6507b0f5543aced
metalwolf <phil@thomassigny.org>
2018-11-06 19:44:32 -0600

    patch %1

--- xconfig.go
+++ xconfig.go
@@ -1,6 +1,7 @@
 package xconfig
 
 import (
+  "fmt"
   "bufio"
   "os"
   "strings"
@@ -9,10 +10,10 @@ import (
 type XConfig map[string]interface{}
 
 func Load(filename string) (*XConfig, error) {
-  config := &XConfig{}
+  config := make(XConfig)
 
   if len(filename) == 0 {
-    return config, nil
+    return &config, nil
   }
   file, err := os.Open(filename)
   if err != nil {
@@ -23,16 +24,23 @@ func Load(filename string) (*XConfig, error) {
   scanner := bufio.NewScanner(file)
   for scanner.Scan() {
     line := scanner.Text()
-    if line[0] == '#' { continue }
+    if len(line) == 0 || line[0] == '#' { continue }
     posequal := strings.Index(line, "=")
     if posequal >= 0 {
       key := strings.TrimSpace(line[:posequal])
       if len(key) > 0 {
-        value := ""
+        var value interface{}
         if len(line) > posequal {
           value = strings.TrimSpace(line[posequal+1:])
+          
+          if (value == "yes" || value == "true") {
+            value = true
+          }
+          if (value == "no" || value == "false") {
+            value = false
+          }
         }
-        (*config)[key] = value
+        config.Set(key, value)
       }
     }
   }
@@ -41,5 +49,19 @@ func Load(filename string) (*XConfig, error) {
     return nil, err
   }
 
-  return config, nil
+  fmt.Println("CONFIG CREADO DESDE XCONFIG:")
+  fmt.Println(config)
+  fmt.Printf("%p\n", &config)
+  fmt.Printf("%p\n", config["mainpage"])
+  
+  return &config, nil
 }
+
+func (c *XConfig) Set(key string, value interface{}) {
+  (*c)[key] = value
+}
+
+func (c *XConfig) Get(key string) interface{} {
+  return (*c)[key]
+}
+
